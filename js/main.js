4ed8cb80e4b12a3b8faf4266305203b650d66c34
Cory Ellerbe <Coco@corys-mbp.lan>
2020-02-22 16:52:32 -0600

    addition of blogger API

--- js/main.js
+++ js/main.js
@@ -23,3 +23,24 @@ window.addEventListener('scroll', function() {
       );
     }
   });
+
+  const url = 'https://www.googleapis.com/blogger/v3/blogs/8857434284299255372/posts?key=AIzaSyC0nZ_oR1VYYYdrOkZbyqTwYhKicSEqIgE'
+  fetch(url)
+    .then( (response) => response.json() )
+    .then((data) => {
+      console.log(data.items);
+      const posts = data.items;
+      return posts.slice(0,3).map(items => {
+        const blogPosts = document.getElementById('blogPosts')
+        const article = document.createElement('article'),
+              title = document.createElement('h2'),
+              content = document.createElement('p')
+
+              title.innerHTML = items.title
+              content.innerHTML = items.content
+
+              article.appendChild(title)
+              article.appendChild(content)    
+              blogPosts.appendChild(article)
+      })
+    })
